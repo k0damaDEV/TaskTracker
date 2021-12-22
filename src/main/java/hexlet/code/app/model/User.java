ef3caadd26e9062aa5addda7fb907533337eaec2
Sergey Ridev <whereareyo@yahoo.com>
2021-12-22 17:17:47 +0300

    db & entity init

---
+++ src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,27 @@
+package hexlet.code.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @CreationTimestamp
+    private Instant createdAt;
+}
